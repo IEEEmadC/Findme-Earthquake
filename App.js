3102e0473f8f62fabe976868d1a9668947aed833
Cristian Ronda <cristian.ronda@epn.edu.ec>
2018-11-07 15:56:27 -0500

    Ready!

--- App.js
+++ App.js
@@ -1,117 +1,141 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow
-//  */
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 
-// import React, {Component} from 'react';
-// import {Platform, StyleSheet, Text, View} from 'react-native';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
-
-// type Props = {};
-// export default class App extends Component<Props> {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>Welcome to React Native!</Text>
-//         <Text style={styles.instructions}>To get started, edit App.js</Text>
-//         <Text style={styles.instructions}>{instructions}</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import MapView, { ProviderPropType, Marker, AnimatedRegion } from 'react-native-maps';
 import  firebase from 'react-native-firebase';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Firebase Ejemplo',
-});
+const screen = Dimensions.get('window');
 
+const ASPECT_RATIO = screen.width / screen.height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
+class AnimatedMarkers extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default class App extends React.Component {
-  // Initialize Firebase
-componentWillMount(){
-  var config = {
-    apiKey: "AIzaSyAg-8uXG2MwUhnP1dVaWDNDeGnLaLpeWQ4",
-    appId: '1:668510197057:android:0215ba072bca4a35',
-    authDomain: "find-me-earthquake.firebaseapp.com",
-    databaseURL: "https://find-me-earthquake.firebaseio.com",
-    projectId: "find-me-earthquake",
-    storageBucket: "find-me-earthquake.appspot.com",
-    messagingSenderId: "668510197057"
-  };
-  firebase.initializeApp(config);
-  firebase.database().ref('texto').set(
-    {
-      name:'Funciona!',
-      edad: 24
+    this.state = {
+      coordinate: new AnimatedRegion({
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+      }),
+    };
+  }
+
+  animate() {
+    const { coordinate } = this.state;
+    const newCoordinate = {
+      latitude: LATITUDE + ((Math.random() - 0.5) * (LATITUDE_DELTA / 2)),
+      longitude: LONGITUDE + ((Math.random() - 0.5) * (LONGITUDE_DELTA / 2)),
+    };
+
+    if (Platform.OS === 'android') {
+      if (this.marker) {
+        this.marker._component.animateMarkerToCoordinate(newCoordinate, 500);
+      }
+    } else {
+      coordinate.timing(newCoordinate).start();
     }
-  ).then(()=>{
-    console.log('INSERTADO')
-  }).catch((error)=>{
-    console.log('ERROR '+error)
-  })
-}
+  }
+
+  componentWillMount(){
+    var config = {
+      apiKey: "AIzaSyAg-8uXG2MwUhnP1dVaWDNDeGnLaLpeWQ4",
+      appId: '1:668510197057:android:0215ba072bca4a35',
+      authDomain: "find-me-earthquake.firebaseapp.com",
+      databaseURL: "https://find-me-earthquake.firebaseio.com",
+      projectId: "find-me-earthquake",
+      storageBucket: "find-me-earthquake.appspot.com",
+      messagingSenderId: "668510197057"
+    };
+    firebase.initializeApp(config);
+    firebase.database().ref('texto').set(
+      {
+        name:'Funciono!',
+        edad: 24
+      }
+    ).then(()=>{
+      console.log('INSERTADO')
+    }).catch((error)=>{
+      console.log('ERROR '+error)
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js!!!</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <MapView
+          provider={this.props.provider}
+          style={styles.map}
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+        >
+          <Marker.Animated
+            ref={marker => { this.marker = marker; }}
+            coordinate={this.state.coordinate}
+          />
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.animate()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text>Animate</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
+AnimatedMarkers.propTypes = {
+  provider: ProviderPropType,
+};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  bubble: {
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
   },
 });
+
+export default AnimatedMarkers;
